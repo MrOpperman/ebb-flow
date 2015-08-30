660fa8e9710b6485b091adc9fb58dd1d6fb7354b
Kyle Opperman <k@kyleo.co.za>
2015-08-30 17:40:50 +0200

    Dont load removed assets

--- js/load.js
+++ js/load.js
@@ -2,8 +2,6 @@ var loadState = {
     preload: function() {
         var loadingLabel = game.add.text(80, 150, 'loading...', { font: '30px Courier', fill: '#ffffff' });
         
-        game.load.image('player', 'assets/player.png');
-        game.load.image('win', 'assets/win.png');
         game.load.spritesheet('mummy', 'assets/metalslug_mummy.png', 37, 45, 18);
     },
     
