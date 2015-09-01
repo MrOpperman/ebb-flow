f81136b0489900eab0a2ee61e8dc5d8aaaab81b4
Kyle Opperman <k@kyleo.co.za>
2015-09-01 19:00:21 +0200

    Stuff

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 var menuState = {
     create: function () {
-        var nameLabel = game.add.text(80, 80, 'EBB & FLOW', { font: '60px Arial', fill: '#ffffff' });
+        var nameLabel = game.add.text(80, 80, 'EBB & FLOW', { font: '60px Arial' });
         
-        var startLabel = game.add.text(80, game.world.height - 80, 'Press the "W" key to start', { font: '25px Arial', fill: '#ffffff' });
+        var startLabel = game.add.text(80, game.world.height - 80, 'Press the "W" key to start', { font: '25px Arial' });
             
         var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
     
--- js/win.js
+++ js/win.js
@@ -1,7 +1,7 @@
 var winState = {
     create: function() {
         var winLabel = game.add.text(80, 80, 'You got ' + counter + ' points!', { font: '50px Arial', fill: '#00FF00' });
-        var startLabel = game.add.text(80, game.world.height-80, 'press the "W" key to restart/', { font: '25px Arial', fill: '#ffffff' });
+        var startLabel = game.add.text(80, game.world.height-80, 'press the "W" key to restart/', { font: '25px Arial'});
         
         var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
         
@@ -11,6 +11,7 @@ var winState = {
     },
     
     restart: function () { 
+        counter = 0;
         game.state.start('menu');   
     }
 }
